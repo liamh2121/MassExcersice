3241823c72d8e577bc18e7adcdce1c10069ca633
Liam <liamh2121@gmail.com>
2022-11-18 20:10:39 +0000

    Updated Regex to use ECMAScript
    Swapped the regular expression to use ECMAScript instead of PCRE2

--- MassExercises/Source.cpp
+++ MassExercises/Source.cpp
@@ -20,7 +20,7 @@ int main() {
 
 	string fileContent;
 	if (readFile(fileContent))
-		showGetDetails(fileContent);
+		showDetails(fileContent);
 	else
 		cout << "File Not Found";
 
@@ -41,12 +41,12 @@ bool readFile(string& fileContent) {
 }
 
 void showDetails(string& fileContent) {
-	regex regex("[<].?[A-Za-z]+[>]\K[^<][A-Za-z]+"); //regular expreasion gets all characters between <> </> tags
+	regex regex("[A-Za-z]+(?=<)"); //regular expreasion gets all characters between <> </> tags
 	smatch matched;
 	
 	regex_search(fileContent, matched, regex);
 
-	cout << "There are " << matched.size()/2 << " people found";
+	cout << "There are " << matched.size()/2 << " people found" << endl;
 
 	for (int i = 0; i <= matched.size() / 2; i++) {
 		cout << matched.str(i) << " " << matched.str(i + 1) << endl;
